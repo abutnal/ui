909365458c5c2d2fc0d4381b4cbd22a7bdd885a9
AB UTNAL <45108373+abutnal@users.noreply.github.com>
2020-01-29 13:40:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>UI</title>
+	<link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+
+	<div class="container-three">
+		<div class="testimonial"> <h1>	UI Design</h1>	</div>
+	</div>
+
+	
+
+	<div class="container-one">
+		<div class="first-row">	
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum veritatis quia, laboriosam delectus, ullam fuga sint esse obcaecati at iste assumenda distinctio ad. Ipsam, porro illum harum distinctio dolor non.
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae ea natus quia, amet illum reprehenderit at quibusdam repudiandae dignissimos quod repellendus quidem incidunt vero numquam sunt eaque vel ipsum illo.
+                    	</p>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum veritatis quia, laboriosam delectus, ullam fuga sint esse obcaecati at iste assumenda distinctio ad. Ipsam, porro illum harum distinctio dolor non.
+                    	</p>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum veritatis quia, laboriosam delectus, ullam fuga sint esse obcaecati at iste assumenda distinctio ad. Ipsam, porro illum harum distinctio dolor non.</p>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum veritatis quia, laboriosam delectus, ullam fuga sint esse obcaecati at iste assumenda distinctio ad. Ipsam, porro illum harum distinctio dolor non.</p>
+
+		</div>
+	</div>
+	<div class="container-two">
+			<div class="col">
+				  <h2>	Title one</h2>
+				 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus delectus fuga doloremque, cumque atque quam debitis doloribus. Explicabo nesciunt, delectus consequatur et aspernatur ea aut tempora obcaecati excepturi dolores rerum!
+			</div>
+			<div class="col">
+				<h2>	Title two</h2>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit labore, autem nesciunt cumque vel quo dolore rem asperiores 
+			</div>
+			<div class="col">
+				<h2>	Title three</h2>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident dolor perferendis adipisci accusantium ea vero praesentium rem repellat, minus quam nemo aperiam unde. 
+			</div>
+	</div>
+
+	<div class="container-three">
+		<div class="testimonial"> <h1>	Your Prefect Artwork Partner</h1>	</div>
+	</div>
+
+  <div class="container-four">	
+				<div class="number-box"><span>1</span></div>
+				<div class="number-box"><span>2</span></div>
+				<div class="number-box"><span>3</span></div>
+				<div class="number-box"><span>4</span></div>
+				<div class="number-box"><span>5</span></div>
+				<div class="number-box"><span>6</span></div>
+				<div class="number-box"><span>7</span></div>
+				<div class="number-box"><span>8</span></div>
+	</div>
+
+</body>
+</html>
